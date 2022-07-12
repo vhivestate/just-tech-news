b0823c8ea39e2c9ab324a18c624c97f9d35d8453
vhivestate <garcia.valeria001@gmail.com>
2022-07-11 23:02:58 -0600

    delete post

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -4,14 +4,15 @@ const { Post, User } = require('../../models');
 // get all users
 router.get('/', (req, res) => {
     Post.findAll({
-      attributes: ['id', 'post_url', 'title', 'created_at'],
-      include: [
-        {
-          model: User,
-          attributes: ['username']
-        }
-      ]
-    })
+        attributes: ['id', 'post_url', 'title', 'created_at'],
+        order: [['created_at', 'DESC']], 
+        include: [
+          {
+            model: User,
+            attributes: ['username']
+          }
+        ]
+      })
       .then(dbPostData => res.json(dbPostData))
       .catch(err => {
         console.log(err);
@@ -71,6 +72,25 @@ router.get('/', (req, res) => {
         }
       }
     )
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        res.json(dbPostData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+  router.delete('/:id', (req, res) => {
+    Post.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
       .then(dbPostData => {
         if (!dbPostData) {
           res.status(404).json({ message: 'No post found with this id' });
