6bd9612164fa5c58fbef29fdeef752a556cb5228
vhivestate <garcia.valeria001@gmail.com>
2022-07-11 22:53:54 -0600

    update post

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -59,5 +59,29 @@ router.get('/', (req, res) => {
         res.status(500).json(err);
       });
   });
-  
+
+  router.put('/:id', (req, res) => {
+    Post.update(
+      {
+        title: req.body.title
+      },
+      {
+        where: {
+          id: req.params.id
+        }
+      }
+    )
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        res.json(dbPostData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
   module.exports = router;
