e7b92c098ec5b8afb85b1efff84bd2a3e426a076
vhivestate <garcia.valeria001@gmail.com>
2022-07-12 22:44:39 -0600

    comment feature

--- models/index.js
+++ models/index.js
@@ -1,6 +1,8 @@
 const User = require('./User');
 const Post = require("./Post");
 const Vote = require('./votes');
+const Comment = require('./Comment');
+
 
 // create associations
 User.hasMany(Post, {
@@ -39,4 +41,20 @@ User.hasMany(Post, {
     foreignKey: 'post_id'
   });
 
-  module.exports = { User, Post, Vote };
+  Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+  });
+  
+  Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+  });
+  
+  User.hasMany(Comment, {
+    foreignKey: 'user_id'
+  });
+  
+  Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+  });
+
+  module.exports = { User, Post, Vote, Comment };
