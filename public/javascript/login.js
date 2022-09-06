40ab29327501c256f7bb8346f8cf828b24d40193
vhivestate <garcia.valeria001@gmail.com>
2022-09-05 19:01:56 -0600

    frontend logic

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,6 +1,19 @@
 function signupFormHandler(event) {
     event.preventDefault();
   
-  }
+    const username = document.querySelector('#username-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
   
-  document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
+    if (username && email && password) {
+      fetch('/api/users', {
+        method: 'post',
+        body: JSON.stringify({
+          username,
+          email,
+          password
+        }),
+        headers: { 'Content-Type': 'application/json' }
+      }).then((response) => {console.log(response)})
+    }
+  }
