5cdc857c39869c36f600956c2183b0aaea69806e
vhivestate <garcia.valeria001@gmail.com>
2022-09-12 17:30:15 -0600

    single post

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -52,4 +52,20 @@ router.get('/', (req, res) => {
     res.render('login');
   });
 
+  router.get('/post/:id', (req, res) => {
+    const post = {
+      id: 1,
+      post_url: 'https://handlebarsjs.com/guide/',
+      title: 'Handlebars Docs',
+      created_at: new Date(),
+      vote_count: 10,
+      comments: [{}, {}],
+      user: {
+        username: 'test_user'
+      }
+    };
+  
+    res.render('single-post', { post });
+  });
+
 module.exports = router;
