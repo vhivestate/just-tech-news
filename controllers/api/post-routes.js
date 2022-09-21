03313377b6a5f12995f29d98fbe6457967289626
vhivestate <garcia.valeria001@gmail.com>
2022-09-20 18:19:04 -0600

    add middleware

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const sequelize = require('../../config/connection');
 const { Post, User, Vote, Comment } = require("../../models");
+const withAuth = require('../../utils/auth');
 
 // get all users
 router.get('/', (req, res) => {
